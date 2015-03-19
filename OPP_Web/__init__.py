eb2b57bea79be7664a2c9b7733f67c484aa7d84a
wo <wo@umsu.de>
2015-03-19 13:57:05 +0100

    layout improvements and pagination

--- OPP_Web/__init__.py
+++ OPP_Web/__init__.py
@@ -1,7 +1,7 @@
 import re
 import MySQLdb.cursors
 import config
-
+from datetime import datetime
 from flask import Flask, request, g, url_for, render_template
 from flaskext.mysql import MySQL
 
@@ -14,10 +14,12 @@ mysql.init_app(app)
 @app.route("/")
 def list_docs():
     cursor = mysql.connect().cursor(MySQLdb.cursors.DictCursor)
-    limit = 20
-    offset = request.args.get('offset') or 0
+    limit = 10
     max_spam = app.config['MAX_SPAM']
     min_confidence = app.config['MIN_CONFIDENCE']
+    before_id = 99999999
+    if request.args.get('before'):
+        before_id = int(request.args.get('before'))
     query = '''
          SELECT
             D.*,
@@ -37,27 +39,62 @@ def list_docs():
             AND S.source_id = R.source_id
             AND spamminess <= %s
             AND meta_confidence >= %s
+            AND D.document_id < %s
             AND L.status = 1
          GROUP BY D.document_id
          ORDER BY D.found_date DESC
          LIMIT %s
-         OFFSET %s
     '''
-    cursor.execute(query, (max_spam, min_confidence, limit, int(offset)))
+    cursor.execute(query, (max_spam, min_confidence, before_id, limit))
     rows = cursor.fetchall()
     for row in rows: 
-        row['title'] = 'asdf'
         row['src'] = row['srcs']
         row['url'] = row['locs']
-        row['short_url'] = 'k'+shorten_url(row['url'])
+        row['short_url'] = shorten_url(row['url'])
+        row['filesize'] = pretty_filesize(row['filesize'])
+        row['reldate'] = relative_date(row['found_date'])
 
     return render_template('list_docs.html', docs=rows)
+
+def pretty_filesize(size):
+    size = int(size)/1000
+    size = "{} KB".format(size)
+    return size
 
 def shorten_url(url):
     url = re.sub(r'^https?://', '', url);
     if len(url) > 60:
         url = url[:28] + '...' + url[-28:]
     return url
+
+def relative_date(time):
+    now = datetime.now()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time,datetime):
+        diff = now - time
+    elif not time:
+        diff = now - now
+    second_diff = diff.seconds
+    day_diff = diff.days
+    
+    if second_diff < 120:
+        return "1&nbsp;minute ago"
+    if second_diff < 3600:
+        return str(second_diff / 60) + "&nbsp;minutes ago"
+    if second_diff < 7200:
+        return "1&nbsp;hour ago"
+    if second_diff < 86400:
+        return str(second_diff / 3600) + "&nbsp;hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + "&nbsp;days ago"
+    if day_diff < 31:
+        return str(day_diff / 7) + "&nbsp;weeks ago"
+    if day_diff < 365:
+        return str(day_diff / 30) + "&nbsp;months ago"
+    return str(day_diff / 365) + "&nbsp;years ago"
 
 if __name__ == "__main__":
     app.run()
