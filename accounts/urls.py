4df8611d84310dec150f173a78d41354152e3868
wo <wo@umsu.de>
2016-07-31 09:15:51 +0200

    bugfixes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
+import django.contrib.auth.views
 from . import views
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^signup_complete/$', views.signup_complete, name='signup_complete'),
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login', 
+    url(r'^login/$', django.contrib.auth.views.login, name='login', 
         kwargs={'template_name': 'accounts/login.html'}),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout',
+    url(r'^logout/$', django.contrib.auth.views.logout, name='logout',
         kwargs={'next_page': '/'}),
 ]
--- opp_web/settings.py
+++ opp_web/settings.py
@@ -71,7 +71,7 @@ MIDDLEWARE_CLASSES = [
 
 # for accesslogger:
 NO_STATS_URLS = ['/media', '/static', '/feed.xml']
-NO_STATS_USERAGENTS = ['dotbot']
+NO_STATS_USERAGENTS = ['dotbot', 'Baiduspider', 'rogerbot', 'RSSingBot', 'feedly']
 
 ROOT_URLCONF = 'opp_web.urls'
 
